a8a45681e2ddf2c148b8ac21d98ed81d4f4c184a
BingXuanOwO <hxc35459@outlook.com>
2022-08-27 00:58:41 +0800

    more readible when have mutiple friend links

--- src/pages/PageFriendLink.vue
+++ src/pages/PageFriendLink.vue
@@ -39,8 +39,9 @@ export default {
     }
     #p-links ul{
         display: flex;
-        padding: 5px;
+        padding: 5px 0;
         flex-wrap: wrap;
+        gap:10px
     }
     .hide{
         opacity: 0;
@@ -57,10 +58,32 @@ export default {
     #p-links ul li{
         display: flex;
         padding: 5px;
-        background: rgb(92, 100, 108);
         border-radius: 50px;
         height: 35px;
-        
+        position: relative;
+    }
+    #p-links ul li span{
+        transition: .3s;
+        opacity: .8;
+    }
+    #p-links ul li:hover span{
+        opacity: 1;
+    }
+    #p-links ul li::after{
+        content: "";
+        height: 100%;
+        width: 100%;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        border-radius: 50px;
+        position: absolute;
+        background: #607082;
+        opacity: .2;
+        transition: .3s;
+    }
+    #p-links ul li:hover::after{
+        opacity: .4;
     }
     #p-links ul li span{
         line-height: 35px;
